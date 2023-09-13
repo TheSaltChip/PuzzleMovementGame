6a56fe7796015868e3adbe7f831d49f72b22b9ee
Sebastian Misje Jonassen <seb2500@gmail.com>
2023-09-13 14:17:45 +0200

    started the alignment fixing

--- Assets/AutoHand/Examples/Scenes/XR/Scripts/XRHandOffset.cs
+++ Assets/AutoHand/Examples/Scenes/XR/Scripts/XRHandOffset.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.XR;
 
 [System.Serializable]
-public struct DeviceData{
+public struct DeviceData
+{
     public string[] deviceNames;
     public Vector3 position;
     public Vector3 rotation;
@@ -14,6 +16,7 @@ public struct DeviceData{
         position = pos;
         rotation = rot;
     }
+
     public DeviceData(string[] names, Vector3 pos, Vector3 rot)
     {
         deviceNames = names;
@@ -23,73 +26,98 @@ public struct DeviceData{
 };
 
 
-public class XRHandOffset : MonoBehaviour {
-    [Tooltip("DO NOT CHANGE THIS UNLESS YOU ARE REDOING THE RELATIVE POSITIONS. This is the device that you are using to setup the innital proper orientation of the hand, all offsets are relative to this device")]
+public class XRHandOffset : MonoBehaviour
+{
+    [Tooltip(
+        "DO NOT CHANGE THIS UNLESS YOU ARE REDOING THE RELATIVE POSITIONS. This is the device that you are using to setup the innital proper orientation of the hand, all offsets are relative to this device")]
     public string defaultDevice = "Oculus";
 
 
-    [SerializeField]
-    public Transform[] rightOffsets, leftOffsets;
+    [SerializeField] public Transform[] rightOffsets, leftOffsets;
 
-    [SerializeField]
-    public DeviceData[] devices = new DeviceData[] {
+    [SerializeField] public DeviceData[] devices =
+    {
         new DeviceData("Oculus", new Vector3(0.005f, -0.016f, 0.014f), new Vector3(48, 0, 15)),
         new DeviceData("Windows MR", new Vector3(0.003f, -0.005f, -0.078f), new Vector3(36, -12, 2)),
-        new DeviceData(new string[]{"Vive", "HTC", "Index", "Cosmos", "Elite" }, new Vector3(0.015f, 0, 0.0412f), new Vector3(30, -17, 0))
+        new DeviceData(new string[] { "Vive", "HTC", "Index", "Cosmos", "Elite" }, new Vector3(0.015f, 0, 0.0412f),
+            new Vector3(30, -17, 0)),
+        new DeviceData("PICO", new Vector3(0.005f, -0.016f, 0.014f), new Vector3(24, 0, 15))
     };
+
+    [SerializeField] private TMP_Text text;
 
     bool offsetDone = false;
     bool hasProvider = false;
 
-    void OnEnable(){
+    void OnEnable()
+    {
         InputDevices.deviceConnected += DeviceConnected;
         List<InputDevice> devices = new List<InputDevice>();
         InputDevices.GetDevices(devices);
 
-        foreach(var device in devices)
+        text.text = "";
+        foreach (var de in devices)
+        {
+            text.text += $"{de.name}\n";
+        }
+
+        foreach (var device in devices)
             DeviceConnected(device);
     }
 
-    void OnDisable() {
-        if(hasProvider) {
+    void OnDisable()
+    {
+        if (hasProvider)
+        {
             InputDevices.deviceConnected -= DeviceConnected;
         }
     }
 
 
-    internal void AdjustPositions(XRHandOffset otherOffset) {
+    internal void AdjustPositions(XRHandOffset otherOffset)
+    {
         var defaultPos = GetDefaultPositionOffset() - otherOffset.GetDefaultPositionOffset();
         var defaultRot = GetDefaultRotationOffset() - otherOffset.GetDefaultRotationOffset();
 
-        foreach(var leftOffset in leftOffsets) {
+        foreach (var leftOffset in leftOffsets)
+        {
             leftOffset.localPosition += new Vector3(-defaultPos.x, defaultPos.y, defaultPos.z);
             leftOffset.localEulerAngles += new Vector3(defaultRot.x, -defaultRot.y, -defaultRot.z);
         }
 
-        foreach(var rightOffset in rightOffsets) {
+        foreach (var rightOffset in rightOffsets)
+        {
             rightOffset.localPosition += defaultPos;
             rightOffset.localEulerAngles += defaultRot;
         }
     }
 
-    void DeviceConnected(InputDevice inputDevice) {
-        if (inputDevice.characteristics != 0){
-            foreach (var device in devices){
+    void DeviceConnected(InputDevice inputDevice)
+    {
+        if (inputDevice.characteristics != 0)
+        {
+            foreach (var device in devices)
+            {
                 if (offsetDone)
                     break;
 
-                for (int i = 0; i < device.deviceNames.Length; i++){
+                for (int i = 0; i < device.deviceNames.Length; i++)
+                {
                     Debug.Log(inputDevice.name, this);
-                    if (inputDevice.name.Contains(device.deviceNames[i])){
+                    if (inputDevice.name.Contains(device.deviceNames[i]))
+                    {
+                        
                         var offsetPos = GetPositionOffset(defaultDevice, device.deviceNames[i]);
                         var offsetRot = GetRotationOffset(defaultDevice, device.deviceNames[i]);
-
-                        foreach (var leftOffset in leftOffsets){
+                        text.text += $"Nice {offsetPos} : {offsetRot}\n";
+                        foreach (var leftOffset in leftOffsets)
+                        {
                             leftOffset.localPosition += new Vector3(-offsetPos.x, offsetPos.y, offsetPos.z);
                             leftOffset.localEulerAngles += new Vector3(offsetRot.x, -offsetRot.y, -offsetRot.z);
                         }
 
-                        foreach (var rightOffset in rightOffsets){
+                        foreach (var rightOffset in rightOffsets)
+                        {
                             rightOffset.localPosition += offsetPos;
                             rightOffset.localEulerAngles += offsetRot;
                         }
@@ -105,16 +133,19 @@ public class XRHandOffset : MonoBehaviour {
     }
 
 
-    Vector3 GetPositionOffset(string from, string to) {
-        if(from == to)
+    Vector3 GetPositionOffset(string from, string to)
+    {
+        if (from == to)
             return Vector3.zero;
 
         Vector3 fromPos, toPos = fromPos = Vector3.zero;
-        foreach(var device in devices) {
-            foreach(var deviceName in device.deviceNames) {
-                if(deviceName == from)
+        foreach (var device in devices)
+        {
+            foreach (var deviceName in device.deviceNames)
+            {
+                if (deviceName == from)
                     fromPos = device.position;
-                if(deviceName == to)
+                if (deviceName == to)
                     toPos = device.position;
             }
         }
@@ -123,16 +154,19 @@ public class XRHandOffset : MonoBehaviour {
     }
 
 
-    Vector3 GetRotationOffset(string from, string to) {
-        if(from == to)
+    Vector3 GetRotationOffset(string from, string to)
+    {
+        if (from == to)
             return Vector3.zero;
 
         Vector3 fromPos, toPos = fromPos = Vector3.zero;
-        foreach(var device in devices) {
-            foreach(var deviceName in device.deviceNames) {
-                if(deviceName == from)
+        foreach (var device in devices)
+        {
+            foreach (var deviceName in device.deviceNames)
+            {
+                if (deviceName == from)
                     fromPos = device.rotation;
-                if(deviceName == to)
+                if (deviceName == to)
                     toPos = device.rotation;
             }
         }
@@ -140,12 +174,14 @@ public class XRHandOffset : MonoBehaviour {
         return (toPos - fromPos);
     }
 
-    protected Vector3 GetDefaultPositionOffset() {
-
+    protected Vector3 GetDefaultPositionOffset()
+    {
         Vector3 fromPos = Vector3.zero;
-        foreach(var device in devices) {
-            foreach(var deviceName in device.deviceNames) {
-                if(deviceName == defaultDevice)
+        foreach (var device in devices)
+        {
+            foreach (var deviceName in device.deviceNames)
+            {
+                if (deviceName == defaultDevice)
                     fromPos = device.position;
             }
         }
@@ -154,11 +190,14 @@ public class XRHandOffset : MonoBehaviour {
     }
 
 
-    protected Vector3 GetDefaultRotationOffset() {
+    protected Vector3 GetDefaultRotationOffset()
+    {
         Vector3 fromPos = Vector3.zero;
-        foreach(var device in devices) {
-            foreach(var deviceName in device.deviceNames) {
-                if(deviceName == defaultDevice)
+        foreach (var device in devices)
+        {
+            foreach (var deviceName in device.deviceNames)
+            {
+                if (deviceName == defaultDevice)
                     fromPos = device.rotation;
             }
         }
