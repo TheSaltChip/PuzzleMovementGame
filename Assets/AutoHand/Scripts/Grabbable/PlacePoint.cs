7d712beb504c413e768cf772631ef7e6905efa6c
Mathias Nupen <584967@stud.hvl.no>
2023-09-20 17:30:42 +0200

    Made a nature scene and attempted to make a puzzle

--- Assets/AutoHand/Scripts/Grabbable/PlacePoint.cs
+++ Assets/AutoHand/Scripts/Grabbable/PlacePoint.cs
@@ -292,10 +292,19 @@ namespace Autohand {
             }
 
             placingFrame = true;
-            originParent = placeObj.transform.parent;
+            originParent = transform.parent;
 
             placeObj.rootTransform.position = placedOffset.position;
-            placeObj.rootTransform.rotation = placedOffset.rotation;
+            //Debug.Log(placedObject.transform.eulerAngles.z);
+            float zRotation =  Mathf.Round( placedObject.transform.eulerAngles.z/ 90)*90;
+            //Debug.Log(zRotation);
+            Transform t = new GameObject().transform;
+            t.eulerAngles = new Vector3(0, 0, zRotation);
+            //Debug.Log(t.rotation.z);
+            //Debug.Log(t.eulerAngles.z);
+            var rot = placedOffset.rotation;
+            rot = Quaternion.Euler(0,rot.y,rot.z);
+            placedObject.transform.rotation = Quaternion.Euler(new Vector3(rot.x-90f, rot.y, t.eulerAngles.z));
 
             if (placeObj.body != null)
             {
