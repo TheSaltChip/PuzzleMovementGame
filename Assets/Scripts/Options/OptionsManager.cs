ebc12d3badf1511e1d15cd0cc68691749f64e222
Sebastian Misje Jonassen <seb2500@gmail.com>
2023-09-21 14:31:32 +0200

    Singleton for managing the auto hand player options
    Switching between snap and continuous turn
    Turn speed and snap angle

---
+++ Assets/Scripts/Options/OptionsManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Autohand;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Options
+{
+    public class OptionsManager : MonoBehaviour
+    {
+        public static OptionsManager Instance;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void SetOptions()
+        {
+            SetTurnOption();
+            SetTurnSpeed();
+            SetSnapTurnAngle();
+        }
+
+        public void SetTurnOption()
+        {
+            if (PlayerPrefs.HasKey("turn"))
+                AutoHandPlayer.Instance.rotationType = (PlayerPrefs.GetInt("turn")) switch
+                {
+                    1 => RotationType.smooth,
+                    _ => RotationType.snap
+                };
+        }
+
+        public void SetTurnSpeed()
+        {
+            if (PlayerPrefs.HasKey("turnSpeed"))
+                AutoHandPlayer.Instance.smoothTurnSpeed = PlayerPrefs.GetFloat("turnSpeed");
+        }
+
+        public void SetSnapTurnAngle()
+        {
+            if (PlayerPrefs.HasKey("snapTurnAngle"))
+                AutoHandPlayer.Instance.snapTurnAngle = PlayerPrefs.GetFloat("snapTurnAngle");
+        }
+    }
+}
