11cc896be9ab7001a47f05056d7d5b35e8eeff53
TheSaltChip <seb2500@gmail.com>
2023-10-02 23:47:31 +0200

    updated start menu

--- Assets/Scripts/GameStartMenu.cs
+++ Assets/Scripts/GameStartMenu.cs
@@ -8,12 +8,10 @@ public class GameStartMenu : MonoBehaviour
     [Header("UI Pages")] public GameObject mainMenu;
 
     public GameObject options;
-    public GameObject about;
 
     [Header("Main Menu Buttons")] public Button startButton;
 
     public Button optionButton;
-    public Button aboutButton;
     public Button quitButton;
 
     public List<Button> returnButtons;
@@ -26,7 +24,6 @@ public class GameStartMenu : MonoBehaviour
         //Hook events
         startButton.onClick.AddListener(StartGame);
         optionButton.onClick.AddListener(EnableOption);
-        aboutButton.onClick.AddListener(EnableAbout);
         quitButton.onClick.AddListener(QuitGame);
 
         foreach (var item in returnButtons) item.onClick.AddListener(EnableMainMenu);
@@ -47,27 +44,17 @@ public class GameStartMenu : MonoBehaviour
     {
         mainMenu.SetActive(false);
         options.SetActive(false);
-        about.SetActive(false);
     }
 
     public void EnableMainMenu()
     {
         mainMenu.SetActive(true);
         options.SetActive(false);
-        about.SetActive(false);
     }
 
     public void EnableOption()
     {
         mainMenu.SetActive(false);
         options.SetActive(true);
-        about.SetActive(false);
-    }
-
-    public void EnableAbout()
-    {
-        mainMenu.SetActive(false);
-        options.SetActive(false);
-        about.SetActive(true);
     }
 }
