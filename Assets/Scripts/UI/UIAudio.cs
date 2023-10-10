8850934f3d4b82d74bfc48d8e5846ad3e60ced05
Sebastian Misje Jonassen <seb2500@gmail.com>
2023-10-10 14:41:31 +0200

    Reorganized files

---
+++ Assets/Scripts/UI/UIAudio.cs
@@ -0,0 +1,28 @@
+using Audio;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    public class UIAudio : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        public string clickAudioName;
+        public string hoverEnterAudioName;
+        public string hoverExitAudioName;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (clickAudioName != "") AudioManager.Instance.Play(clickAudioName);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (hoverEnterAudioName != "") AudioManager.Instance.Play(hoverEnterAudioName);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (hoverExitAudioName != "") AudioManager.Instance.Play(hoverExitAudioName);
+        }
+    }
+}
