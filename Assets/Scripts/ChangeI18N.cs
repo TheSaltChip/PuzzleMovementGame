8c59a38d48f76fb8be5c3b8cf80e07431b0c422d
TheSaltChip <seb2500@gmail.com>
2023-10-13 12:49:38 +0200

    SerializeField on the Toggle as this is what you actually want

--- Assets/Scripts/ChangeI18N.cs
+++ Assets/Scripts/ChangeI18N.cs
@@ -1,23 +1,14 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
 using UnityEngine.UI;
 
 public class ChangeI18N : MonoBehaviour
 {
-    private Toggle _active;
-    [SerializeField] private GameObject toggle;
-
-    private void Start()
-    {
-        _active = toggle.GetComponent<Toggle>();
-    }
+    [SerializeField] private Toggle active;
 
     public void ChangeLanguage()
     {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-        LocalizationSettings.SelectedLocale = _active.isOn
+        LocalizationSettings.SelectedLocale = active.isOn
             ? LocalizationSettings.AvailableLocales.Locales[0]
             : LocalizationSettings.AvailableLocales.Locales[1];
     }
