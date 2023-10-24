2fc0aac279dc5ae2e00c77652d8a6e42f4b3d1c4
Sebastian Misje Jonassen <seb2500@gmail.com>
2023-10-24 14:36:39 +0200

    Fps counter on the wrist menu

---
+++ Assets/Scripts/Util/Fps.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Util
+{
+    public class Fps : MonoBehaviour
+    {
+        private float count;
+        private WaitForSeconds _waitForSeconds;
+        [SerializeField] private TMP_Text text;
+
+        private IEnumerator Start()
+        {
+            _waitForSeconds = new WaitForSeconds(0.1f);
+            while (true)
+            {
+                count = 1f / Time.unscaledDeltaTime;
+                yield return _waitForSeconds;
+            }
+        }
+
+        private void OnGUI()
+        {
+            text.text = $"FPS: {Mathf.Round(count)}";
+        }
+    }
+}
