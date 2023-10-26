00f0e830e3ae3f3196e940b80aafbbcae6826bb4
TheSaltChip <seb2500@gmail.com>
2023-10-27 00:33:23 +0200

    Audio player components for completables

---
+++ Assets/Scripts/Audio/CompletableAudioPlayer.cs
@@ -0,0 +1,32 @@
+﻿using Level.Completables;
+using UnityEngine;
+
+namespace Audio
+{
+    [RequireComponent(typeof(Completable), typeof(AudioSource))]
+    public class CompletableAudioPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioSource source;
+        [SerializeField] private Completable completable;
+        [SerializeField] private AudioClip doneClip;
+        [SerializeField] private AudioClip failedClip;
+
+        private void Awake()
+        {
+            completable.OnDone.AddListener(PlayOnDoneSound);
+            completable.OnFailedCheck.AddListener(PlayOnFailedSound);
+        }
+
+        private void PlayOnDoneSound()
+        {
+            source.clip = doneClip;
+            source.Play();
+        }
+        
+        private void PlayOnFailedSound()
+        {
+            source.clip = failedClip;
+            source.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/CompletableRandomAudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using Autohand;
+using Level.Completables;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Audio
+{
+    [RequireComponent(typeof(Completable), typeof(AudioSource))]
+    public class CompletableRandomAudioPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioSource source;
+        [SerializeField] private Completable button;
+        [SerializeField] private AudioClip[] clips;
+
+        private void Awake()
+        {
+            button.OnDone.AddListener(PlayRandomClickSound);
+        }
+
+        private void PlayRandomClickSound()
+        {
+            source.clip = clips[Random.Range(0, clips.Length)];
+            source.Play();
+        }
+    }
+}
