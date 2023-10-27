3e0c757acfe9fb7c45dd7eea2c93ab08c2bf7a4a
TheSaltChip <seb2500@gmail.com>
2023-10-28 01:16:48 +0200

    refactored logic

--- Assets/Scripts/Level/Completables/ColorRecognition/ColorRecognitionBoardCompletable.cs
+++ Assets/Scripts/Level/Completables/ColorRecognition/ColorRecognitionBoardCompletable.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Text;
-using Audio;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace Level.Completables.ColorRecognition
@@ -15,19 +12,26 @@ namespace Level.Completables.ColorRecognition
         [SerializeField] private int patternLength;
         [SerializeField] private float blinkDuration;
 
-        private List<ColorButtonCompletable> _buttons;
+        private ColorButtonCompletable[] _buttons;
         private List<int> _pattern;
         private int _patternIndex;
 
-        private int[] _isInPattern;
+        private bool[] _isInPattern;
 
         private void Awake()
         {
             _pattern = new List<int>();
 
-            _buttons = new List<ColorButtonCompletable>();
-            _buttons.AddRange(gameObject.GetComponentsInChildren<ColorButtonCompletable>());
-            _isInPattern = new int[_buttons.Count];
+            _buttons = gameObject.GetComponentsInChildren<ColorButtonCompletable>();
+            _isInPattern = new bool[_buttons.Length];
+        }
+
+        public void ResetValues()
+        {
+            _pattern.Clear();
+
+            _buttons = gameObject.GetComponentsInChildren<ColorButtonCompletable>();
+            _isInPattern = new bool[_buttons.Length];
         }
 
         public void CreatePattern()
@@ -44,13 +48,13 @@ namespace Level.Completables.ColorRecognition
 
         private void CreateRepeatingPattern()
         {
-            var count = _buttons.Count;
+            var count = _buttons.Length;
 
             for (var i = 0; i < patternLength; i++)
             {
                 var num = Random.Range(0, count);
                 _pattern.Add(num);
-                _isInPattern[num]++;
+                _isInPattern[num] = true;
             }
 
             var sb = new StringBuilder();
@@ -67,59 +71,46 @@ namespace Level.Completables.ColorRecognition
 
         private void CreateNonRepeatingPattern()
         {
-            var count = _buttons.Count;
+            var count = _buttons.Length;
 
             for (var i = 0; i < patternLength; i++)
             {
-                if (_pattern.Count == _buttons.Count) return;
+                if (_pattern.Count == _buttons.Length) return;
 
                 var num = Random.Range(0, count);
 
                 if (_pattern.Contains(num)) continue;
 
                 _pattern.Add(num);
-                _isInPattern[num]++;
+                _isInPattern[num] = true;
             }
         }
 
 
         private void CheckCompletion()
         {
-            for (var i = 0; i < _buttons.Count; i++)
+            for (var buttonIndex = 0; buttonIndex < _buttons.Length; buttonIndex++)
             {
-                if (
-                    _isInPattern[i] > 0 // Button is in the pattern 
-                    && _buttons[i].IsDone // and is pressed 
-                    && _pattern[_patternIndex] == i // and the current button is the next in the pattern
-                )
-                {
-                    _buttons[i].ResetState();
-                    _patternIndex++;
-
-                    if (_patternIndex == _pattern.Count)
-                    {
-                        Completed();
-                        return;
-                    }
-
-                    continue;
-                }
-
-
-                // Button is not in the pattern or is not pressed or is not the current button
-
-                if (_isInPattern[i] == 0 && _buttons[i].IsDone) // Button is not in the pattern and is pressed
+                if ((!_isInPattern[buttonIndex] // Button is not in the pattern
+                     // Button is in the pattern but not the next one
+                     || (_isInPattern[buttonIndex] && _pattern[_patternIndex] != buttonIndex))
+                    && _buttons[buttonIndex].IsDone) // Button is pressed
                 {
                     Failed();
                     return;
                 }
 
-                if (_isInPattern[i] > 0 && _pattern[_patternIndex] != i &&
-                    _buttons[i].IsDone) // Button is in the pattern and pressed but not the next one 
-                {
-                    Failed();
-                    return;
-                }
+                if (!_isInPattern[buttonIndex]
+                    || !_buttons[buttonIndex].IsDone
+                    || _pattern[_patternIndex] != buttonIndex) continue;
+
+                _buttons[buttonIndex].ResetState();
+                _patternIndex++;
+
+                if (_patternIndex != _pattern.Count) continue;
+
+                Completed();
+                return;
             }
 
             OnIncompleteCheck.Invoke();
