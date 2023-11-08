8020a4122331d967c5b34d579704aeecdabe6bc6
TheSaltChip <seb2500@gmail.com>
2023-11-08 21:15:20 +0100

    SO for int and float variables

---
+++ Assets/Scripts/Variables/IntReference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variables
+{
+    [Serializable]
+    public class IntReference
+    {
+        public bool UseConstant = true;
+        public int ConstantValue;
+        public IntVariable Variable;
+
+        public int Value => UseConstant ? ConstantValue : Variable.value;
+    }
+}
