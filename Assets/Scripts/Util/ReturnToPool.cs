6a3ca9d3883f86e52ec58d38f7e47a10bf8ca1de
TheSaltChip <seb2500@gmail.com>
2023-11-08 21:15:00 +0100

    util for returning to a pool

---
+++ Assets/Scripts/Util/ReturnToPool.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Util
+{
+    public class ReturnToPool : MonoBehaviour
+    {
+        public IObjectPool<GameObject> pool;
+
+        public void Return()
+        {
+            // Return to the pool
+
+            if (!gameObject.activeSelf) return;
+            pool?.Release(gameObject);
+        }
+    }
+}
