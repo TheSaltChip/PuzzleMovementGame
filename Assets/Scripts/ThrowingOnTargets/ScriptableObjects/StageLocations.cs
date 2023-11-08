65d132be90d46197ff10ad51d5d5b3123b3e34b5
TheSaltChip <seb2500@gmail.com>
2023-11-08 21:14:27 +0100

    SO for stages and locations and rotations for a given "stage"
    Will most likely be moved to a more general directory

---
+++ Assets/Scripts/ThrowingOnTargets/ScriptableObjects/StageLocations.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ThrowingOnTargets.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "StageLocations", menuName = "Target/StageLocations")]
+    public class StageLocations : ScriptableObject
+    {
+        public Vector3[] locations;
+        public Vector3[] eulerRotations;
+        
+    }
+}
---
+++ Assets/Scripts/ThrowingOnTargets/ScriptableObjects/Stages.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Variables;
+
+namespace ThrowingOnTargets.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "TargetStages", menuName = "Target/Stages")]
+    public class Stages : ScriptableObject
+    {
+        public IntReference currentStage;
+        public StageLocations[] stages;
+
+        public StageLocations CurrentStage()
+        {
+            return stages[Mathf.Clamp(currentStage.Value, 0, stages.Length-1)];
+        }
+    }
+}
