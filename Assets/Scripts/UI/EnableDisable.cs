ae696f95e761c25d6e28af1d0bf63a101cb84a4c
TheSaltChip <seb2500@gmail.com>
2023-11-24 16:04:31 +0100

    enables and disables the gameobject it is on

---
+++ Assets/Scripts/UI/EnableDisable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class EnableDisable : MonoBehaviour
+    {
+        public void ChangeState()
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+
+        public void Enable()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Disable()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
