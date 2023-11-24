4aa53e6dcd5e82cb06f0431f0c508f6195b0966b
TheSaltChip <seb2500@gmail.com>
2023-11-24 16:05:12 +0100

    String variable and reference scriptableobjects

---
+++ Assets/Scripts/Variables/StringReference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variables
+{
+    [Serializable]
+    public class StringReference
+    {
+        public bool UseConstant = true;
+        public string ConstantValue;
+        public StringVariable Variable;   
+        
+        public string Value => UseConstant ? ConstantValue : Variable.value;
+    }
+}
