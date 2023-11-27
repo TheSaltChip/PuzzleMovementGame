5210f04e754f4db292c585f5e3bfe7c9ff2c5f46
TheSaltChip <seb2500@gmail.com>
2023-11-27 22:46:23 +0100

    util component for triggering events

---
+++ Assets/Scripts/Util/ActivateEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Util
+{
+    public class ActivateEvent : MonoBehaviour
+    {
+        [SerializeField] private bool value;
+        
+        public UnityEvent activate;
+
+        private void Update()
+        {
+            if (!value) return;
+            
+            value = false;
+            activate.Invoke();
+        }
+    }
+}
