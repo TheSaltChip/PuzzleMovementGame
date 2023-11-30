62e8985f73d473428d8014b39fa807430da30cd9
TheSaltChip <seb2500@gmail.com>
2023-12-01 00:57:14 +0100

    Should be working, not tested though

--- Assets/Scripts/ThrowingOnTargets/Difficulty/DifficultySetter.cs
+++ Assets/Scripts/ThrowingOnTargets/Difficulty/DifficultySetter.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using ThrowingOnTargets.Saveable;
 using ThrowingOnTargets.ScriptableObjects;
 using UnityEngine;
-using UnityEngine.Android;
-using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 namespace ThrowingOnTargets.Difficulty
 {
@@ -17,22 +17,73 @@ namespace ThrowingOnTargets.Difficulty
             switch (difficulty)
             {
                 case ThrowLevelDifficulty.Easy:
-                    CreateStage(3, 4, 6);
+                    CreateStages(3, 4, 6);
                     break;
                 case ThrowLevelDifficulty.Medium:
-                    CreateStage(5, 6, 10);
+                    CreateStages(5, 6, 10);
                     break;
                 case ThrowLevelDifficulty.Hard:
-                    CreateStage(7, 8, 13);
+                    CreateStages(7, 8, 13);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(difficulty), difficulty, null);
             }
         }
 
+        private void CreateStages(int numStages, int minTargetsPerStage, int maxTargetsPerStage)
+        {
+            var gridLength = targetInfo.grid.Length;
+            var pos = new List<int>(numStages);
+            throwLevel.stages = new Stage[numStages];
+            
+            for (var i = 0; i < numStages; i++)
+            {
+                var rand = Random.Range(minTargetsPerStage, maxTargetsPerStage + 1);
+
+                for (var j = 0; j < rand; j++)
+                {
+                    if (pos.Count == rand) return;
+
+                    while (true)
+                    {
+                        var num = Random.Range(0, gridLength);
+
+                        if (pos.Contains(num)) continue;
+
+                        pos.Add(num);
+                        break;
+                    }
+                }
+
+                var stage = new Stage { posRots = new PosRotScl[pos.Count] };
+
+                for (var j = 0; j < pos.Count; j++)
+                {
+                    stage.posRots[j] = new PosRotScl
+                    {
+                        location = targetInfo.grid[pos[j]],
+                        rotation = new Vector3(90, 0, 180),
+                        scale = Vector3.one
+                    };
+                }
+
+                throwLevel.stages[i] = stage;
+            }
+        }
+
         public void SetEasyDifficulty()
         {
             SetDifficulty(ThrowLevelDifficulty.Easy);
+        }
+        
+        public void SetMediumDifficulty()
+        {
+            SetDifficulty(ThrowLevelDifficulty.Medium);
+        }
+        
+        public void SetHardDifficulty()
+        {
+            SetDifficulty(ThrowLevelDifficulty.Hard);
         }
 
         private const float LengthBetweenClosestTargets = 0.65f;
@@ -46,101 +97,19 @@ namespace ThrowingOnTargets.Difficulty
 
         /*
          * 1: 1
-         * 2: 6 - 60° per target
-         * 3: 12 - 30° per target
-         * 4: 24 - 15° per target
+         * 2: 6 - 0 step turn
+         * 3: 12 - 1 step turn
+         * 4: 18 - 2 step turn
+         * 5: 24 - 3 step turn
+         *
+         * 1   2    3    4
+         * 7 - 19 - 37 - 61
+         *   12   18   24
+         *   2*6  3*6  4*6
+         *
+         * 1 + 6*1 | 1 + 6*1 + 6*2 | 1 + 6*1 + 6*2 + 6*3 | 1 + 6*1 + 6*2 + 6*3 + 6*4
+         * 7              19               37                   61
          */
-
-        private void CreateStage(int numStages, int minTargets, int maxTargets)
-        {
-            var stages = new Stage[numStages];
-
-            var center = targetInfo.spawnPoint;
-
-            var ring1 = (int)(6 * Mathf.Pow(2, 0));
-            var ring2 = (int)(6 * Mathf.Pow(2, 1));
-            var ring3 = (int)(6 * Mathf.Pow(2, 2));
-            var places = new Vector3[1 + ring1 + ring2];
-
-
-            places[0] = center;
-            
-            for (var i = 0; i < ring1; i++)
-            {
-                places[i+1] = center +
-                            new Vector3(
-                                LengthBetweenClosestTargets * Mathf.Cos(i * Mathf.PI / 3f),
-                                LengthBetweenClosestTargets * Mathf.Sin(i * Mathf.PI / 3f),
-                                0);
-            }
-
-            var step = 0;
-
-            var firstPointRing2 = center;
-            
-            for (var i = 0; i < ring2; i++)
-            {
-                var secondPoint =firstPointRing2 +
-                                  new Vector3(
-                                      LengthBetweenClosestTargets * Mathf.Cos(step * Mathf.PI / 3f),
-                                      LengthBetweenClosestTargets * Mathf.Sin(step * Mathf.PI / 3f),
-                                      0);
-                
-                firstPointRing2 = secondPoint;
-                
-                if (i != 0 && i % 2 == 0)
-                {
-                    ++step;
-                }
-
-                places[ring1 + i + 1] = center + secondPoint;
-                
-            }
-
-            var firstPointRing3 = new Vector3(
-                LengthBetweenCenterAnd3rdRing,
-                LengthBetweenCenterAnd3rdRing,
-                0);
-
-            print(ring3 - ring2 - ring1);
-            print(ring1);
-            print(ring2);
-            print(ring3);
-            /*for (var i = 0; i < ring3; i++)
-            {
-                var secondPoint = firstPointRing3 +
-                                  new Vector3(
-                                      LengthBetweenClosestTargets * Mathf.Cos(step * Mathf.PI / 3f),
-                                      LengthBetweenClosestTargets * Mathf.Sin(step * Mathf.PI / 3f),
-                                      0);
-
-                if (i != 0 && i % 3 == 0)
-                {
-                    ++step;
-                    firstPointRing3 = secondPoint;
-                }
-                
-                places[ring2 + i] = center + secondPoint;
-            }*/
-            
-            var stage = new Stage { posRots = new PosRotScl[places.Length] };
-
-            for (var i = 0; i < places.Length; i++)
-            {
-                stage.posRots[i] = new PosRotScl()
-                {
-                    location = places[i],
-                    rotation = new Vector3(90, 0, 180),
-                    scale = Vector3.one
-                };
-            }
-
-
-            throwLevel.stages = new[]
-            {
-                stage
-            };
-        }
     }
 
     public enum ThrowLevelDifficulty
