6e34c2aa55cf66c81c54371358d8e1fca3b64599
TheSaltChip <seb2500@gmail.com>
2023-12-08 13:33:30 +0100

    refact'ed

--- Assets/Scripts/Lobby/LevelInfoManager.cs
+++ Assets/Scripts/Lobby/LevelInfoManager.cs
@@ -1,40 +1,34 @@
-using System.IO;
 using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 namespace Lobby
 {
     public class LevelInfoManager : MonoBehaviour
     {
-        [SerializeField] private SceneInfo sceneInfo;
         [SerializeField] private GameObject buttonPrefab;
-        private string _filePath;
-        void Awake()
+
+        private void Awake()
         {
             LoadLevelNames();
         }
-    
+
         public void LoadLevelNames()
         {
-        
             var sceneCount = SceneManager.sceneCountInBuildSettings;
-            var vertLayout = gameObject.GetComponent<VerticalLayoutGroup>();
-            print(sceneCount);
+
             for (var i = 1; i < sceneCount; i++)
             {
                 var sceneName = SceneUtility.GetScenePathByBuildIndex(i)
                     .Split('/')
                     .Last()
                     .Split('.')[0];
-                var button = GameObject.Instantiate(buttonPrefab,gameObject.transform);
+
+                var button = Instantiate(buttonPrefab, gameObject.transform);
                 button.SetActive(true);
                 button.GetComponentInChildren<TMP_Text>().text = sceneName;
             }
-
-        
         }
     }
 }
