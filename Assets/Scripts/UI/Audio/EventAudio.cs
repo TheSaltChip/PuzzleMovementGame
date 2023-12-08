eff183e88b0c4fc1b4527595eeafee827196589d
TheSaltChip <seb2500@gmail.com>
2023-12-08 19:17:48 +0100

    SO for storing UI audio clips

---
+++ Assets/Scripts/UI/Audio/EventAudio.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UI.Audio
+{
+    [CreateAssetMenu(fileName = "EventAudio", menuName = "Audio/EventAudio")]
+    public class EventAudio : ScriptableObject
+    {
+        public AudioClip onPointerClick;
+        public AudioClip onPointerHoverEnter;
+        public AudioClip onPointerHoverExit;
+    }
+}
