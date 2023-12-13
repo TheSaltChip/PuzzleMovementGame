b2f6a6b8ccbbd47a696ea984a2f815d5381b9c16
Sebastian Misje Jonassen <seb2500@gmail.com>
2023-12-13 16:15:45 +0100

    created

---
+++ Assets/Scripts/Memorization/Figure/FigureMatchChecker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Memorization.Figure
+{
+    public class FigureMatchChecker : MonoBehaviour
+    {
+        /* TODO: Use selected figures SO to check if the selected figures are matching
+         * Check if the number of selected items are equal to a limit
+         * Subscribe to an event which fires whenever a figure has been selected
+         * If the items are matching delete and remove from list
+         * if not reenable collider and remove from list
+         */
+    }
+}
