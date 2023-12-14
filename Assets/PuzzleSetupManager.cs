e50fafc023cd4fe65c40fb8d4046d1e66c6016b6
Mathias Nupen <584967@stud.hvl.no>
2023-12-14 15:58:39 +0100

    some work

--- Assets/PuzzleSetupManager.cs
+++ Assets/PuzzleSetupManager.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleSetupManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int height;
+    [SerializeField] private int width;
+    
+    [SerializeField] private GameObject puzzlePiece;
+    [SerializeField] private GameObject board;
+    [SerializeField] private GameObject placePoint;
+
+    private Vector3 ppSize;
+
+    private void Awake()
     {
-        
+        ppSize = placePoint.GetComponent<Renderer>().bounds.size;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlacePieces()
     {
-        
+        var trpp = placePoint.transform;
+        var rot = trpp.rotation;
+        var pos = trpp.position;
+        var amount = height * width;
+        var grid = new Vector3[height,width];
+        var posY = -height / 2;
+        var posX = -width / 2;
+        if (amount % 2 == 0)
+        {
+            var posEven = new Vector3(pos.x-ppSize.x,pos.y-ppSize.y,pos.z);
+            for (var i = 0; i < height; i++)
+            {
+                posX = 0;
+                for (var j = 0; j < width; j++)
+                {
+                    grid[i, j] = new Vector3(posEven.x + posX*ppSize.x-ppSize.x/2,posEven.y + posY*ppSize.y-ppSize.y/2,pos.z);
+                    posX++;
+                }
+
+                posY--;
+            }
+        }
+        else
+        {
+            
+        }
     }
 }
