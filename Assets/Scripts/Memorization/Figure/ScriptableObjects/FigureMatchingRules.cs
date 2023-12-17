28960df87e7ac24839c14f6a92bc20bf49df4d54
TheSaltChip <seb2500@gmail.com>
2023-12-17 13:58:59 +0100

    moved totalAmountOfFigures to spawn into here

--- Assets/Scripts/Memorization/Figure/ScriptableObjects/FigureMatchingRules.cs
+++ Assets/Scripts/Memorization/Figure/ScriptableObjects/FigureMatchingRules.cs
@@ -1,5 +1,6 @@
 ﻿using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Memorization.Figure.ScriptableObjects
 {
@@ -8,12 +9,31 @@ namespace Memorization.Figure.ScriptableObjects
     {
         public MatchingRule matchingRule;
 
-        [HideIf("matchingRule", MatchingRule.Figure)]
-        public int numColor;
+        [HideIf("matchingRule", MatchingRule.Figure), Range(1, 10)]
+        public int maxNumColor;
 
-        [HideIf("matchingRule", MatchingRule.Color)]
-        public int numFigure;
+        [HideIf("matchingRule", MatchingRule.Color), Range(1, 16)]
+        public int maxNumFigure;
 
-        public int numToMatch;
+        [Range(2, 5)] public int numToMatch;
+
+        [SerializeField, Range(1, 96),
+         Tooltip("This number will be changed to fit the criteria: (this num) % numberOfFiguresToMatch == 0")]
+        private int totalNumberOfFigures;
+
+        public int TotalTotalNumberOfFigures
+        {
+            get
+            {
+                if (totalNumberOfFigures % numToMatch != 0)
+                {
+                    totalNumberOfFigures -= totalNumberOfFigures % numToMatch;
+                }
+
+                return totalNumberOfFigures;
+            }
+
+            set => totalNumberOfFigures = value;
+        }
     }
 }
