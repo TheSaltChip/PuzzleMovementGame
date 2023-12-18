d9c9bb138c15e0f7d350bf89b66e7555424601b9
TheSaltChip <seb2500@gmail.com>
2023-12-18 22:50:33 +0100

    Create FigureGameStateWatcher.cs

---
+++ Assets/Scripts/Memorization/Figure/FigureGameStateWatcher.cs
@@ -0,0 +1,28 @@
+﻿using Memorization.Figure.ScriptableObjects;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+using Variables;
+
+namespace Memorization.Figure
+{
+    public class FigureGameStateWatcher : MonoBehaviour
+    {
+        [SerializeField] private FigureMatchingRules rules;
+        [SerializeField] private IntVariable figuresLeft;
+
+        public UnityEvent levelDone;
+        
+        public void Setup()
+        {
+            figuresLeft.value = rules.TotalTotalNumberOfFigures;
+        }
+
+        public void CorrectMatched()
+        {
+            figuresLeft.value -= rules.NumToMatch;
+
+            if (figuresLeft.value <= 0) levelDone?.Invoke();
+        }
+    }
+}
