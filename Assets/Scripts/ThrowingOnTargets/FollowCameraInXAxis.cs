a7134ff8e97b536dd4bf42d60a160a1ec7f1c3e3
TheSaltChip <seb2500@gmail.com>
2024-01-22 15:16:40 +0100

    made it so the ball dispenser follows the main camera in the x axis

---
+++ Assets/Scripts/ThrowingOnTargets/FollowCameraInXAxis.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ThrowingOnTargets
+{
+    public class FollowCameraInXAxis : MonoBehaviour
+    {
+        private Transform _mainCameraTransform;
+
+        private void Start()
+        {
+            SetCameraTransform();
+        }
+
+        public void SetCameraTransform()
+        {
+            var mainCamera = GameObject.FindWithTag("MainCamera");
+            _mainCameraTransform = mainCamera == null ? transform : mainCamera.transform;
+        }
+
+        private void Update()
+        {
+            var pos = transform.position;
+            transform.position = new Vector3(_mainCameraTransform.position.x, pos.y, pos.z);
+        }
+    }
+}
