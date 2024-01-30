0c818395895961a8dfbf8be5dc4fa12a07bc50cf
Sebastian Misje Jonassen <seb2500@gmail.com>
2024-01-30 14:51:37 +0100

    Started the new spawning system

---
+++ Assets/Scripts/ThrowingOnTargets/ScriptableObjects/ThrowLevelRules.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ThrowingOnTargets.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "ThrowLevelRules", menuName = "ThrowAtTargets/ThrowLevelRules")]
+    public class ThrowLevelRules : ScriptableObject
+    {
+        public int stages;
+        public int targetsPerStage;
+        public int xSize;
+        public int ySize;
+        public float chanceBigTarget;
+        public float distBetweenStages;
+    }
+}
--- Assets/Scripts/ThrowingOnTargets/StageSpawner.cs
+++ Assets/Scripts/ThrowingOnTargets/StageSpawner.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using ThrowingOnTargets.ScriptableObjects;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Pool;
+using UnityEngine.Serialization;
 using Util;
 using Variables;
 
@@ -11,16 +13,32 @@ namespace ThrowingOnTargets
 {
     public class StageSpawner : MonoBehaviour
     {
-        [SerializeField] private ThrowLevelSO throwLevel;
+        [FormerlySerializedAs("throwLevel")] [SerializeField]
+        private ThrowLevelRules throwLevelRules;
+
         [SerializeField] private GameObject target;
         [SerializeField] private IntVariable targetsInStage;
         [SerializeField] private TargetInfo targetInfo;
 
         public UnityEvent onStageReady;
-        
+
         private IObjectPool<GameObject> _targets;
         private Coroutine _setupCoroutine;
         private WaitForSeconds _waitFor100Milliseconds;
+
+        private struct Plane
+        {
+            public int[][] XYIndices;
+        }
+
+        private struct Depths
+        {
+            public Plane[] Planes;
+        }
+
+        private Depths _spawnablePositions;
+
+        private float _length = 0.5f + 1f;
 
         private void Awake()
         {
@@ -38,9 +56,9 @@ namespace ThrowingOnTargets
             _waitFor100Milliseconds = new WaitForSeconds(0.1f);
         }
 
-        public void SetupStage()
+        public void OldSetupStage()
         {
-            var stageLocations = throwLevel.CurrentStage();
+            /*var stageLocations = throwLevel.CurrentStage();
 
             var posRots = stageLocations.posRots;
 
@@ -48,12 +66,107 @@ namespace ThrowingOnTargets
             {
                 Debug.LogError("No positions for targets found");
                 return;
-            };
-            
+            }
+
             targetsInStage.value = posRots.Length;
 
             StartCoroutine(SetStageCoroutine(posRots));
-            
+
+            onStageReady?.Invoke();*/
+        }
+
+        public void SetupStage()
+        {
+            _spawnablePositions = new Depths
+            {
+                Planes = new Plane[throwLevelRules.stages]
+            };
+
+            for (var i = 0; i < throwLevelRules.stages; i++)
+            {
+                _spawnablePositions.Planes[i].XYIndices = new int[throwLevelRules.xSize][];
+                var index = 0;
+
+                for (var j = 0; j < throwLevelRules.xSize; j++)
+                {
+                    _spawnablePositions.Planes[i].XYIndices[j] = new int[throwLevelRules.ySize];
+                    for (var k = 0; k < throwLevelRules.ySize; k++)
+                    {
+                        _spawnablePositions.Planes[i].XYIndices[j][k] = index++;
+                    }
+                }
+            }
+
+            var posRotScls = new List<PosRotScl>();
+            var usedIndices = new List<int>();
+
+            for (var i = 0; i < throwLevelRules.stages; i++)
+            {
+                var plane = _spawnablePositions.Planes[i];
+
+                for (var j = 0; j < throwLevelRules.targetsPerStage; j++)
+                {
+                    var isBig = Random.value < throwLevelRules.chanceBigTarget;
+                    int index;
+                    bool newLoop;
+
+                    do
+                    {
+                        newLoop = false;
+                        var x = Random.Range(0, throwLevelRules.xSize);
+                        var y = Random.Range(0, throwLevelRules.ySize);
+
+                        index = plane.XYIndices[x][y];
+                        
+                        // TODO - Check if a big target can be placed, turn into not big if true
+                        if (!isBig) continue;
+
+                        var isLegalBigStartIndex =
+                            index >= throwLevelRules.xSize - 1 || index >= throwLevelRules.ySize - 1;
+
+                        if (isLegalBigStartIndex)
+                        {
+                            newLoop = true;
+                            continue;
+                        }
+
+                        var isAnIndexTaken =
+                            usedIndices.Contains(index)
+                            || usedIndices.Contains(plane.XYIndices[x + 1][y])
+                            || usedIndices.Contains(plane.XYIndices[x][y + 1])
+                            || usedIndices.Contains(plane.XYIndices[x + 1][y + 1]);
+
+                        if (isAnIndexTaken)
+                        {
+                            newLoop = true;
+                            continue;
+                        }
+
+                        usedIndices.AddRange(new[]
+                        {
+                            plane.XYIndices[x + 1][y],
+                            plane.XYIndices[x][y + 1],
+                            plane.XYIndices[x + 1][y + 1]
+                        });
+                        break;
+                    } while (usedIndices.Contains(index) || newLoop);
+
+                    usedIndices.Add(index);
+
+                    posRotScls.Add(new PosRotScl
+                    {
+                        // ReSharper disable once PossibleLossOfFraction
+                        location = new Vector3(
+                            (index / throwLevelRules.xSize) * 0.75f - throwLevelRules.xSize * 0.75f / 2 + 0.75f * 0.5f,
+                            (index % throwLevelRules.ySize) * 0.75f - throwLevelRules.ySize * 0.75f / 2 + 0.75f * 0.5f,
+                            i * throwLevelRules.distBetweenStages),
+                        rotation = new Vector3(-90, 0, 0)
+                    });
+                }
+            }
+
+            StartCoroutine(SetStageCoroutine(posRotScls));
+
             onStageReady?.Invoke();
         }
 
