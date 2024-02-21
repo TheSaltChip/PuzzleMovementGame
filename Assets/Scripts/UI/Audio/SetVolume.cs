e0d4db5cf6c209a6fb285b2d3d78a34c2a2fe282
Sebastian Misje Jonassen <seb2500@gmail.com>
2024-02-21 12:18:59 +0100

    things

---
+++ Assets/Scripts/UI/Audio/SetVolume.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Audio
+{
+    public class SetVolume : MonoBehaviour
+    {
+        [SerializeField] private Slider slider;
+
+        private void Start()
+        {
+            var volume = PlayerPrefs.GetFloat("MasterVolume", 0.50f);
+            slider.value = volume;
+            AudioListener.volume = volume;
+        }
+        
+        public void SetLevel(float sliderValue)
+        {
+            AudioListener.volume = sliderValue;
+            PlayerPrefs.SetFloat("MasterVolume", sliderValue);
+        }
+    }
+}
