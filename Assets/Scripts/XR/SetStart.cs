6bf20031be3e3a2ca66fb6ec6b81f772e01b54d4
Sebastian Misje Jonassen <seb2500@gmail.com>
2024-02-27 13:12:09 +0100

    added spawnpoints

---
+++ Assets/Scripts/XR/SetStart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace XR
+{
+    public class SetStart : MonoBehaviour
+    {
+        private void Start()
+        {
+            var g = GameObject.FindWithTag("SpawnPoint");
+
+            transform.position = g.transform.position;
+            transform.rotation = g.transform.rotation;
+        }
+    }
+}
