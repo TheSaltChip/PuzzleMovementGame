64a0a8f086c29d083d98edf2b921b3638f570675
Sebastian Misje Jonassen <seb2500@gmail.com>
2024-03-20 12:11:49 +0100

    should set selected button upon page selection

---
+++ Assets/Scripts/UI/ThrowOnTarget/SetSelectedButtonFromFloatVariable.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using Events.String;
+using UnityEngine;
+using Variables;
+
+namespace UI.ThrowOnTarget
+{
+    public class SetSelectedButtonFromFloatVariable : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable id;
+
+        public StringGameEvent buttonGroupGameEvent;
+
+        public void Raise()
+        {
+            buttonGroupGameEvent.Raise(id.value.ToString("0.##",CultureInfo.InvariantCulture));
+        }
+    }
+}
