66bd901badffba0b63ce00ca73f4118efb2fc487
Sebastian Misje Jonassen <seb2500@gmail.com>
2024-03-21 13:46:15 +0100

    fixed an edge case for scene transition

--- Assets/Scripts/Lobby/UITeleport.cs
+++ Assets/Scripts/Lobby/UITeleport.cs
@@ -13,10 +13,18 @@ namespace Lobby
         [SerializeField, ShowIf("staticGate")] private StringVariable targetSceneName;
 
         public UnityEvent<string> sceneNameToTeleportTo;
-
+        
+        private bool _active;
         public void ChangeScene()
         {
+            if (!_active && !staticGate) return;
+            
             sceneNameToTeleportTo?.Invoke(staticGate ? targetSceneName.Value : sceneInfo.sceneName);
+        }
+        
+        public void ActivateGate()
+        {
+            _active = true;
         }
     }
 }
